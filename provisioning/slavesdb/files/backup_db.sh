7628268f585ba6cb1ed5178cb89e5374c01693b1
Dmitriy_P <dimonkmv@yandex.ru>
2024-10-06 21:05:14 +0300

    commit_06_09

---
+++ provisioning/slavesdb/files/backup_db.sh
@@ -0,0 +1,1 @@
+mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers > /home/vagrant/db_backup/db.sql
